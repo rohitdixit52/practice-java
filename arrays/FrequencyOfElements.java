801e677488263923699ce03b230c75daba754624
rohitdixit52 <dixitrohit665@gmail.com>
2026-01-02 10:40:14 +0530

    Added frequency of elements program using array

---
+++ arrays/FrequencyOfElements.java
@@ -0,0 +1,37 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class FrequencyOfElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        boolean[] visited = new boolean[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("\nElement : Frequency");
+        for (int i = 0; i < n; i++) {
+            if (visited[i])
+                continue;
+
+            int count = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+            System.out.println(arr[i] + " : " + count);
+        }
+
+        sc.close();
+    }
+}
